9d3fdd81ad2bbafa2d8f29d32ad1c5c7693615bc
WilsonReese <reese@chicagobooth.edu>
2020-12-01 00:19:19 +0000

    assigning tickets works, getting tickets, confirms reservation

--- app/controllers/ticket_requests_controller.rb
+++ app/controllers/ticket_requests_controller.rb
@@ -30,7 +30,6 @@ class TicketRequestsController < ApplicationController
 
     #need to get ticket from API
     
-
     # to get it to create the first ticket -> user_id is current user, group_id is from the reservation, status is assigned, and ticket is produced
     the_ticket_request.ticket = "QR CODE FOR FIRST TICKET"
     if the_ticket_request.valid?
@@ -51,31 +50,40 @@ class TicketRequestsController < ApplicationController
       end
       tickets_made += 1
     end
+    the_group_reservation.reservation_status = "confirmed"
+    the_group_reservation.save
     redirect_to("/ticket_requests", { :notice => "Ticket requests created successfully." })
-
-    # if the_ticket_request.valid?
-    #   the_ticket_request.save
-    #   redirect_to("/ticket_requests", { :notice => "Ticket request created successfully." })
-    # else
-    #   redirect_to("/ticket_requests", { :notice => "Ticket request failed to create successfully." })
-    # end
   end
 
   def update
     the_id = params.fetch("path_id")
     the_ticket_request = TicketRequest.where({ :id => the_id }).at(0)
+    query_email = params.fetch("query_user_email")
 
-    the_ticket_request.user_id = params.fetch("query_user_id")
-    the_ticket_request.group_id = params.fetch("query_group_id")
-    the_ticket_request.ticket_status = params.fetch("query_ticket_status")
-    the_ticket_request.ticket = params.fetch("query_ticket")
-
-    if the_ticket_request.valid?
-      the_ticket_request.save
-      redirect_to("/ticket_requests/#{the_ticket_request.id}", { :notice => "Ticket request updated successfully."} )
+    if User.exists?(:email => query_email)
+      the_ticket_request.user_id = User.where({ :email => query_email }).first.id
+      the_ticket_request.ticket_status = "assigned"
+      if the_ticket_request.valid?
+        the_ticket_request.save
+        redirect_to("/ticket_requests/#{the_ticket_request.id}", { :notice => "Ticket assigned to #{query_email}."} )
+      else
+        redirect_to("/ticket_requests/#{the_ticket_request.id}", { :alert => "Ticket request failed to update successfully." })
+      end
     else
-      redirect_to("/ticket_requests/#{the_ticket_request.id}", { :alert => "Ticket request failed to update successfully." })
+      redirect_to("/ticket_requests/#{the_ticket_request.id}", { :alert => "There is no user with the email: #{query_email}" })
     end
+    
+    # the_ticket_request.user_id = params.fetch("query_user_id")
+    # the_ticket_request.group_id = params.fetch("query_group_id")
+    # the_ticket_request.ticket_status = params.fetch("query_ticket_status")
+    # the_ticket_request.ticket = params.fetch("query_ticket")
+
+    # if the_ticket_request.valid?
+    #   the_ticket_request.save
+    #   redirect_to("/ticket_requests/#{the_ticket_request.id}", { :notice => "Ticket request updated successfully."} )
+    # else
+    #   redirect_to("/ticket_requests/#{the_ticket_request.id}", { :alert => "Ticket request failed to update successfully." })
+    # end
   end
 
   def destroy
